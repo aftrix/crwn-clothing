739d3c21a344f5a2487d345dc5152c1aed1487da
Afthab Ahmed <aahmed@asnr.org>
2020-10-10 06:22:52 -0500

    added custom button module and form inputs

---
+++ src/pages/account/account.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './account.style.scss';
+
+const AccountPage = () => (
+    <div className='account'>
+        <SignIn />
+    </div>
+);
+
+export default AccountPage;
