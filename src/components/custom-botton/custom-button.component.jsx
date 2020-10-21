6337e7632bafac83b172e6556b06adf29dc65244
Afthab Ahmed <aahmed@asnr.org>
2020-10-21 14:24:04 -0500

    Sign up style modified

--- src/components/custom-botton/custom-button.component.jsx
+++ src/components/custom-botton/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './custom-button.style.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({ children, isGoogleSignIn, isLiteBtn, ...otherProps }) => (
     <button 
-        className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+        className={`${isGoogleSignIn ? 'google-sign-in ' : ''}${isLiteBtn ? 'lite ' : ''}custom-button`} {...otherProps}>
         {children}
     </button>
 );
