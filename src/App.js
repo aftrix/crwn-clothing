4e5d308bc09592c35b0a70001fd16c08beaffcb1
aftrix <aftab.php@gmail.com>
2020-11-12 17:14:20 -0600

    Upload Shop Data to Firebase using AddCollectionAndDocs Util

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ import AccountPage from './pages/account/account.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { auth,
+  createUserProfileDocument
+} from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -19,7 +22,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
