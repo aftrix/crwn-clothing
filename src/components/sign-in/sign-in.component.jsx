d751a2b8858eb00499e262f063d0b3d54dfee6cb
aftrix <aftab.php@gmail.com>
2020-11-25 20:45:56 -0600

    Moved Google SignIn to redux-saga

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-botton/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.style.scss';
 
@@ -39,6 +41,7 @@ class SignIn extends React.Component{
     }
 
     render() {
+        const { googleSignInStart } = this.props;
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -62,8 +65,10 @@ class SignIn extends React.Component{
 
                     <div className='buttons'>
                         <CustomButton type='submit'> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                            Sign in with Google
+                        <CustomButton
+                            type='button'
+                            onClick={googleSignInStart} isGoogleSignIn>
+                                Sign in with Google
                         </CustomButton>
                     </div>
                 </form>
@@ -72,4 +77,8 @@ class SignIn extends React.Component{
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
