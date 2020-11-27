822e2338cb7ec8b833a60cf45dbba519403761ab
aftrix <aftab.php@gmail.com>
2020-11-26 21:31:06 -0600

    Sign-in upon sign-up success

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -20,11 +20,13 @@ const userReducer = (state = INITIAL_STATE, action) => {
                 error: null
             }
         case UserActionTypes.SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_UP_FAILURE:
         case UserActionTypes.SIGN_OUT_FAILURE:
             return {
                 ...state,
                 error: action.payload
             }
+        
         default:
             return state;
     }
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -22,9 +22,9 @@ import { auth,
   getCurrentUser
 } from '../../firebase/firebase.utils';
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
   try {
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(createUserProfileDocument, userAuth, additionalData);
     const userSnapshot = yield userRef.get();
     yield put(signInSuccess({
       id: userSnapshot.id,
@@ -81,6 +81,10 @@ export function* signUp({payload: { email, password, displayName }}) {
   }
 }
 
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+  yield getSnapshotFromUserAuth(user, additionalData);
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -98,7 +102,11 @@ export function* onSignOutStart() {
 }
 
 export function* onSignUpStart() {
-  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp)
+  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
 }
 
 export function* userSagas() {
@@ -107,6 +115,7 @@ export function* userSagas() {
     call(onEmailSignInStart),
     call(onCheckUserSession),
     call(onSignOutStart),
-    call(onSignUpStart)
+    call(onSignUpStart),
+    call(onSignUpSuccess)
   ])
 }
